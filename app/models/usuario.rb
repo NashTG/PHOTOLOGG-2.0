f8389573152efd16f739c462266b2a9fcf4dbfcc
Nash Toledo <itoledogattoni@gmail.com>
2015-10-01 23:01:11 +0200

    login bueno

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,7 +2,9 @@ class Usuario < ActiveRecord::Base
   self.table_name = "USUARIO"
 
   def authenticate(contrasena)
-		if ((self.CONTRASENA).eql? contrasena)
+    sql = "Call mostrarContrasena(#{self.ID_USUARIO})"
+    pass = ActiveRecord::Base.connection.exec_query(sql)
+		if ((pass.rows.first.first).eql? contrasena)
 			return true
 		else
 			return false
